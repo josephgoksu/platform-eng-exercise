382d1b1c8555d8a75785d3f66a930341199c444f
josephgoksu <josephgoksu@gmail.com>
2023-05-31 09:25:48 +0100

    sec: apply security practices to ecr module

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -1,4 +1,18 @@
-# Create an ECR repository named "my-ecr-repo"
+# Create an ECR repository named <project_name>-ecr-repo
 resource "aws_ecr_repository" "ecr_repo" {
+  # Name of the ECR repository
   name = "${var.project_name}-ecr-repo"
+
+  # Set the image tag mutability to immutable
+  image_tag_mutability = "IMMUTABLE"
+
+  # Enable image scanning on push
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  # Add tags to the ECR repository
+  tags = {
+    Name = "${var.project_name}-ecr-repo"
+  }
 }
